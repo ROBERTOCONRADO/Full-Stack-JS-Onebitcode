5d8353d192c0d8262ffed2ec3ff43034fbc0db0e
RobertoConrado <robertoconradorc@gmail.com>
2022-08-28 14:10:57 -0300

    if

---
+++ if else if/script.js
@@ -0,0 +1,26 @@
+
+
+function interacao() {
+    alert('Neste exercício vamos simular o sistema de navegação de uma nave interagindo com o usuário.');
+    let piloto = prompt('Qual o seu nome piloto?');
+    let velocity = prompt(`${piloto}: Qual velocidade gostaria de acelerar a nave?`);
+    let confirmation = confirm(`Vamos acelerar a nave a ${velocity} KM/S! Você confirma?`);
+    if (confirmation == true) {
+        let naveVelocity = velocity
+        if (naveVelocity <= 0) {
+        alert('A nave está parada. Considere partir e aumentar a velocidade.');    
+        } else if (naveVelocity < 40) {
+        alert('Você está devagar, podemos aumentar mais!');
+        } else if (naveVelocity >= 40 && naveVelocity <= 80) {
+        alert('Parece uma boa velocity para manter.');
+        } else if (naveVelocity > 80 && naveVelocity <= 100){
+        alert('Velocidade alta. Considere diminuir.');
+        } else if (naveVelocity > 100) {
+        alert('Velocidade perigosa. Controle automático forçado.');
+        }
+    } else {
+        alert(`A nave foi desligada ${piloto}`);
+    }
+    
+}
+interacao();
