220785f7580765718cf5b252259239fa0137d535
RobertoConrado <robertoconradorc@gmail.com>
2022-08-31 11:45:22 -0300

    functions

--- Exercitando_Funçoes/script.js
+++ Exercitando_Funçoes/script.js
@@ -1,54 +1,57 @@
-let naveNome = prompt('Digite o nome da nave!');
-let naveVelocidade = 0
-let chosenOption
+function start()  { 
 
-function menu() {
-    let option
-    while(option != '1' && option != '2' && option != '3' && option != '4') {
-        option = prompt(`O que deseja fazer?\n
-                        1- Acelerar a nave em 5km/s\n
-                        2- Desacelerar a nave em 5km/s\n
-                        3- Imprimir dados de bordo\n
-                        4- Sair do programa`)
+ 
+    let naveNome = prompt('Digite o nome da nave!');
+    let naveVelocidade = 0
+    let chosenOption
+
+    function menu() {
+        let option
+        while(option != '1' && option != '2' && option != '3' && option != '4') {
+            option = prompt(`O que deseja fazer?\n
+                            1- Acelerar a nave em 5km/s\n
+                            2- Desacelerar a nave em 5km/s\n
+                            3- Imprimir dados de bordo\n
+                            4- Sair do programa`)
+        }
+        return option
     }
-    return option
-}
 
-function acelerar(velocidade) {
-    let novaVelocidade = velocidade + 5
-    return novaVelocidade
-}
-
-function desacelerar(velocidade) {
-    let novaVelocidade = velocidade - 5
-    if(novaVelocidade < 0) {
-        novaVelocidade = 0
+    function acelerar(velocidade) {
+        let novaVelocidade = velocidade + 5
+        return novaVelocidade
     }
-    return novaVelocidade
-}
 
-function imprimirDados(nome, velocidade) {
-    alert(`Espaçonave: ${nome}\nVelocidade: ${velocidade} km/s`);
-}
-
-do {
-    chosenOption = menu()
-    switch(chosenOption) {
-        case '1':
-            naveVelocidade = acelerar(naveVelocidade);
-            break;
-        case '2':
-            naveVelocidade = desacelerar(naveVelocidade);
-            break;
-        case '3':
-            imprimirDados(naveNome, naveVelocidade);
-            break;
-        default:
-            alert('Encerrando programa de bordo!')    
+    function desacelerar(velocidade) {
+        let novaVelocidade = velocidade - 5
+        if(novaVelocidade < 0) {
+            novaVelocidade = 0
+        }
+        return novaVelocidade
     }
-} while(chosenOption != '4')
 
+    function imprimirDados(nome, velocidade) {
+        alert(`Espaçonave: ${nome}\nVelocidade: ${velocidade} km/s`);
+    }
 
+    do {
+        chosenOption = menu()
+        switch(chosenOption) {
+            case '1':
+                naveVelocidade = acelerar(naveVelocidade);
+                break;
+            case '2':
+                naveVelocidade = desacelerar(naveVelocidade);
+                break;
+            case '3':
+                imprimirDados(naveNome, naveVelocidade);
+                break;
+            default:
+                alert('Encerrando programa de bordo!')    
+        }
+    } while(chosenOption != '4')
+
+} 
 
 /*Neste exercício vamos simular o computador de bordo de uma espaçonave no momento em que ela inicia a parada até abrir as comportas para os pilótos saírem*/
 function ativar() {
@@ -66,6 +69,6 @@ function ativar() {
     let spaceshipVelocity = 150
 
     slowDown(spaceshipVelocity, function(velocity) {
-        console.log(`Velocidade atual: ${velocity}`)
+        alert(`Velocidade atual: ${velocity}`)
     }) 
 }
