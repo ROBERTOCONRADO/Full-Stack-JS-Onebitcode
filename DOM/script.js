31357737118a984a5dee4aeee0494da94ad00997
RobertoConrado <robertoconradorc@gmail.com>
2022-09-01 12:52:00 -0300

    Functions

--- DOM/script.js
+++ DOM/script.js
@@ -1,14 +1,24 @@
 function show() {
     let element = document.getElementById('name');
-    console.log(element.value);
+    alert(element.value);
 }
 
 function chamar1() {
     let element = document.getElementsByName('telefone');
-    console.log(element[0].value);
+    alert(element[0].value);
 }
 
 function chamar2() {
     let element = document.getElementsByName('telefone');
-    console.log(element[1].value);
+    alert(element[1].value);
+}
+
+function chamar4() {
+    let element = document.querySelectorAll("div#contatos input[name='telefone']");
+    alert(element[0].value);
+}
+
+function chamar5() {
+    let element = document.getElementsByTagName('input')
+    alert(element[2].value);
 }
