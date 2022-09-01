e650c58c27f85457c3ea4cd50083f3cfdf2e1bb3
RobertoConrado <robertoconradorc@gmail.com>
2022-09-01 15:58:57 -0300

    functions

--- DOM/Manipulando o DOM/script.js
+++ DOM/Manipulando o DOM/script.js
@@ -2,13 +2,18 @@ function addNovoTelefone() {
     const formPhone = document.querySelector('form#telefones');
     const novoTel = formPhone.children[0].cloneNode(true);
     const posicaoTell = formPhone.children.length + 1;
-    novoTel.querySelector("label").innerText = "Telefone " + posicaoTell +": "
+    novoTel.querySelector("label").innerText = `Telefone: ${posicaoTell}`;
     formPhone.appendChild(novoTel);
     console.log(formPhone.children);
 }
 
 
 
-function ImprimirTell() {
-
+function imprimirTell() {
+    let message = ""
+    const phones = document.querySelectorAll("input[name='tell']");
+    phones.forEach((tell, index) => {
+        message += `Telefone ${index + 1}: ${tell.value}\n`
+    })
+    alert(message)
 }
