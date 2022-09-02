ddef400a8516490ea86559c6a2684dc666e74f4c
RobertoConrado <robertoconradorc@gmail.com>
2022-09-02 17:32:48 -0300

    function

--- Seletores Formularios/script.js
+++ Seletores Formularios/script.js
@@ -1,3 +1,10 @@
 function showInfo() {
-    
+    let name = document.querySelector("input[name='name']").value
+    let favoriteColor = document.querySelector("select[name='color'] option:checked").text
+    let likePrograming = document.querySelector("input[name='like-programing']:checked").value
+    let developerOptions = document.querySelectorAll("input[name='developer-options']:checked")
+    let optionsValue = []
+    developerOptions.forEach(Element => {optionsValue.push(Element.value)})
+    let optionsChecked = optionsValue.join(", ")
+    alert(`Nome: ${name}\nCor Primária: ${favoriteColor}\nGosta de Programar? ${likePrograming}\nConhecimentos em Programação: ${optionsChecked}`)
 }
