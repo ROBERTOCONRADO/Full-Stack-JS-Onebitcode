c9a8f6173a91d60c270337e4814a6b5695479f42
RobertoConrado <robertoconradorc@gmail.com>
2022-09-03 15:33:47 -0300

    manipulando_css

--- Manipulando Estilos/script.js
+++ Manipulando Estilos/script.js
@@ -5,18 +5,30 @@ function funcao1() {
     element.style.padding = "25px"
     element.style.textShadow = "2px 2px white"
     element.style.fontHeigth = "bold"
-    element.style.transform = "rotate(100deg)"
+    element.style.transform = "rotate(10deg)"
     element.style.animation = "1s infinite alternate"
-    
-    
 }
 function funcao2() {
     let element = document.getElementById("style-text");
     element.classList.add("teste")
 }
 function funcao3() {
-
+    let element = document.getElementById("style-text");
+    element.classList.remove("teste")
 }
 function funcao4() {
-
+    let element = document.getElementById("style-text");
+    element.classList.add("text-shadow-pop-tl")
+}
+function funcao5() {
+    let element = document.getElementById("style-text");
+    element.classList.remove("text-shadow-pop-tl")
+}
+function funcao6() {
+    let element = document.getElementById("style-text");
+    element.classList.add("atention")
+}
+function funcao7() {
+    let element = document.getElementById("style-text");
+    element.classList.remove("atention")
 }
