3bc368c2effe68fdfcc9b0a540ff9a6514dec804
RobertoConrado <robertoconradorc@gmail.com>
2022-09-03 15:51:15 -0300

    css

--- Manipulando Estilos/script.js
+++ Manipulando Estilos/script.js
@@ -1,12 +1,11 @@
 function funcao1() {
     let element = document.getElementById("style-text");
     element.style.borderRadius = "3px"
-    element.style.backgroundColor = "blue"
+    element.style.backgroundColor = "#124578"
     element.style.padding = "25px"
     element.style.textShadow = "2px 2px white"
     element.style.fontHeigth = "bold"
     element.style.transform = "rotate(10deg)"
-    element.style.animation = "1s infinite alternate"
 }
 function funcao2() {
     let element = document.getElementById("style-text");
